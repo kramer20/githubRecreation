ca3bc61b5bf18db60b57fe1c5b8d8411aa839d56
Malorie Kramer <maloriekramer@Malorie-Kramers-MacBook-Pro.local>
2016-06-24 14:48:00 -0400

    api work

--- js/github.js
+++ js/github.js
@@ -6,13 +6,15 @@ $( document ).ready(function() {
   	type:"GET",
   	success: function (myhub) {
 
-  		 var github = myhub.key;
-
-  		 github.forEach(function (profile){
-    	$(".rightCol").append('<div class="col-lg-3 col-md-3 contentRight"><div class="picBox"><img class="myProfPic" src="'+profile.avatar_url+'"></div><div class="userInfo"></div></div>');
-    	});
-
-  		}
+      var joined = moment(myhub.created_at).format("MMM D, YYYY");
+      var followers = myhub.followers;
+      var following = myhub.following;
+      var email = myhub.email;
+      var login = myhub.login;
+      var name = myhub.name;
+  		var profile = myhub.avatar_url;
+    	$(".rightCol").append('<div class="col-lg-3 col-md-3 contentRight"><div class="picBox"><img class="myProfPic" src="'+profile+'"></div><div class="userInfo"><p class="name">'+name+'</p><p class="login">'+login+'</p><hr><a href="mailto:mkramer2089@gmail.com" class="email">'+email+'</a><p class="joined">Joined on '+joined+'</p><hr><a href="https://github.com/kramer20/followers" class="followers">'+followers+' Follower</a><a href="https://github.com/stars" class="starred">0 Starred</a><a href="https://github.com/kramer20/following" class="following">'+following+' Following</a></div></div>');
+      }
   	});	
 
 	//code for repositories etc.
