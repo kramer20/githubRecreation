2ba3898b2342272b723718d5d7e57c51b508e73e
Malorie Kramer <maloriekramer@Malorie-Kramers-MacBook-Pro.local>
2016-06-26 10:25:31 -0400

    styling part one

--- js/github.js
+++ js/github.js
@@ -13,13 +13,13 @@ $( document ).ready(function() {
       var login = myhub.login;
       var name = myhub.name;
   		var profile = myhub.avatar_url;
-    	$(".rightCol").append('<div class="col-lg-3 col-md-3 contentRight"><div class="picBox"><img class="myProfPic" src="'+profile+'"></div><div class="userInfo"><p class="name">'+name+'</p><p class="login">'+login+'</p><hr><a href="mailto:mkramer2089@gmail.com" class="email">'+email+'</a><p class="joined">Joined on '+joined+'</p><hr><a href="https://github.com/kramer20/followers" class="followers">'+followers+' Follower</a><a href="https://github.com/stars" class="starred">0 Starred</a><a href="https://github.com/kramer20/following" class="following">'+following+' Following</a></div></div>');
+    	$(".rightCol").append('<div class="col-lg-3 col-md-3 contentRight"><div class="picBox"><img class="myProfPic" src="'+profile+'"></div><div class="userInfo"><p class="name">'+name+'</p><p class="login">'+login+'</p><hr><a href="mailto:mkramer2089@gmail.com" class="email">'+email+'</a><p class="joined">Joined on '+joined+'</p><hr><div class="col-lg-4 <a href="https://github.com/kramer20/followers" class="followers">'+followers+' Follower</a><a href="https://github.com/stars" class="starred">0 Starred</a><a href="https://github.com/kramer20/following" class="following">'+following+' Following</a></div></div>');
       }
   	});	
 
 	//code for repositories
 	$.ajax({
-  	url: ' https://api.github.com/users/kramer20/repos',
+  	url: 'https://api.github.com/users/kramer20/repos',
   	type:"GET",
   	success: function (repositories) {
 
@@ -27,10 +27,19 @@ $( document ).ready(function() {
     var repo = repositories;
     var updated = moment(repositories.updated_at).startOf('hour').fromNow();      
     repo.forEach(function (info) {
-      $(".repoSection").append('<div class="col-lg-9 repoLeft"><a href="repositories.html_url" class="repoName">'+info.name+'</a><p class="updated">'+updated+'</p><hr></div><div class="col-lg-3 repoRight"><ul class="repoList"><li class="language">'+info.language+'</li><li class="star">'+info.stargazers_count+'</li><li class="fork">'+info.forks_count+'<hr></ul>');
+      $(".repoSection").append('<div class="col-lg-9 repoLeft"><a href="repositories.html_url" class="repoName">'+info.name+'</a><p class="updated">'+updated+'</p></div><hr><div class="col-lg-3 repoRight"><ul class="repoList"><li class="language">'+info.language+'</li><li class="star">'+info.stargazers_count+'</li><li class="fork">'+info.forks_count+'</li></ul></div><hr>');
     });
 
   		}
   	});	
 
+
+  //code for activity tab
+
+    $.ajax({
+    url: 'https://api.github.com/users/kramer20/events',
+    type:"GET",
+    success: function (activities) {
+    }
+  });
 });
